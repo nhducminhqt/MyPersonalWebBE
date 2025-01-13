36f7046eb0141cb355280890b066cb6da30194ef
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-01-13 16:48:34 +0700

    done product

--- src/main/java/com/example/demo/Controller/ProductController.java
+++ src/main/java/com/example/demo/Controller/ProductController.java
@@ -1,4 +1,49 @@
 package com.example.demo.Controller;
 
+import com.example.demo.dtos.CategoryDTO;
+import com.example.demo.dtos.ProductDTO;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+@RestController
+@RequestMapping("api/v1/products")
 public class ProductController {
+    @GetMapping("")
+    public ResponseEntity<?> getProduct(
+            @RequestParam("page") int page,
+            @RequestParam("limit") int limit,
+            @Valid @RequestBody ProductDTO productDTO,BindingResult result
+
+    ){
+        if(result.hasErrors()){
+            List<String> errorMessages = result.getFieldErrors().stream().map(FieldError::getDefaultMessage).toList();
+            return ResponseEntity.badRequest().body(errorMessages);
+        }
+    return  ResponseEntity.ok("getProducts here" +productDTO);
+    }
+    @PostMapping("")
+    public ResponseEntity<?> createProducts(@Valid @RequestBody ProductDTO productDTO,
+                                              BindingResult result){
+        if(result.hasErrors()){
+            List<String> errorMessages = result.getFieldErrors().stream().map(FieldError::getDefaultMessage).toList();
+            return ResponseEntity.badRequest().body(errorMessages);
+        }
+        return ResponseEntity.ok("This is post med"+productDTO);
+    }
+    @GetMapping("/{id}")
+    public ResponseEntity<String> getProductById(@PathVariable("id") String productId
+    ){
+        return  ResponseEntity.ok("getProducts here"+productId);
+    }
+    @DeleteMapping("/{id}")
+    public ResponseEntity<String> deleteProductById(@PathVariable Long id
+    ){
+        return  ResponseEntity.ok("del Products here"+id);
+    }
 }
+
+
---
+++ src/main/java/com/example/demo/dtos/ProductDTO.java
@@ -0,0 +1,24 @@
+package com.example.demo.dtos;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+import com.google.auto.value.extension.serializable.SerializableAutoValue;
+import jakarta.validation.constraints.*;
+import lombok.*;
+
+@Data
+@Getter
+@NoArgsConstructor
+@AllArgsConstructor
+@Setter
+public class ProductDTO {
+    @NotBlank(message = "Title is require")
+    @Size(min=3,max=200,message = "Title must be between 3 and 200 characters")
+    private String name;
+    @Min(value = 0, message = "Price must >0")
+    @Max(value = 10000000, message = "Price must be < 10.000.000")
+    private Float price;
+    private String thumbnail;
+    private String description;
+    @JsonProperty("category_id")
+    private String categoryId;
+}
