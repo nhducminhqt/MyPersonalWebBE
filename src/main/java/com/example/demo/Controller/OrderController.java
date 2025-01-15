e515dbaf53f84cbc94ca7c205b6323a8d6a7b463
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-01-15 16:36:31 +0700

    add Order Controller

--- src/main/java/com/example/demo/Controller/OrderController.java
+++ src/main/java/com/example/demo/Controller/OrderController.java
@@ -1,9 +1,33 @@
 package com.example.demo.Controller;
 
+import com.example.demo.dtos.OrderDTO;
+import jakarta.validation.Valid;
 import lombok.*;
 import org.checkerframework.checker.units.qual.A;
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+import java.util.List;
+
+@RestController
+@RequestMapping("api/v1/orders")
 public class OrderController {
-
+    @PostMapping("")
+    public ResponseEntity<?> createOrder(@RequestBody @Valid OrderDTO orderDTO,
+                                         BindingResult result) {
+        try{
+            if(result.hasErrors()){
+                List<String> errorMessages = result.getFieldErrors().stream().map(FieldError::getDefaultMessage).toList();
+                return ResponseEntity.badRequest().body(errorMessages);
+            }
+            return ResponseEntity.ok().body(orderDTO);
+        }catch (Exception e){
+            return ResponseEntity.badRequest().body(e.getMessage());
+        }
+    }
 }
