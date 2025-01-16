fbab6e2057b69e50b265a7c8a68b1217aa7b8350
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-01-16 16:03:27 +0700

    add validateion for Order Detail DTO

--- src/main/java/com/example/demo/dtos/OrderDetailDTO.java
+++ src/main/java/com/example/demo/dtos/OrderDetailDTO.java
@@ -1,6 +1,7 @@
 package com.example.demo.dtos;
 
 import com.fasterxml.jackson.annotation.JsonProperty;
+import jakarta.validation.constraints.Min;
 import lombok.*;
 
 @Data
@@ -10,12 +11,17 @@ import lombok.*;
 @Setter
 public class OrderDetailDTO {
     @JsonProperty("order_id")
+    @Min(value = 1,message = "Order id must be > 0")
     private Long orderId;
     @JsonProperty("product_id")
+    @Min(value = 1,message = "Product id must be > 0")
     private String productId;
+    @Min(value = 0,message = "Price must be > 0")
     private float price;
     @JsonProperty("number_of_products")
+    @Min(value = 1,message = "number_of_products must be > 0")
     private int numberOfProducts;
+    @Min(value = 0,message = "Total money must be > 0")
     @JsonProperty("total_money")
     private float totalMoney;
     private String color;
