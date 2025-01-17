b35151c34d7e5bd936299dbdce9adde2e29fb30c
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-01-17 14:20:40 +0700

    add entity category

--- src/main/java/com/example/demo/models/category.java
+++ src/main/java/com/example/demo/models/category.java
@@ -1,4 +1,20 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="categories")
+@Data
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
 public class category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @Column(name="name", nullable=false)
+    private String name;
+
 }
