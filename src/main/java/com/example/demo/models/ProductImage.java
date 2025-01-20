6c66fe859612d27d69e802fba6889d23914366fa
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-01-20 12:49:32 +0700

    add product image

--- src/main/java/com/example/demo/models/ProductImage.java
+++ src/main/java/com/example/demo/models/ProductImage.java
@@ -1,4 +1,21 @@
 package com.example.demo.models;
 
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name="product_images")
+@Data
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
 public class ProductImage {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    @ManyToOne
+    @JoinColumn(name="product_id")
+    private Product product;
+    private String imageUrl;
 }
