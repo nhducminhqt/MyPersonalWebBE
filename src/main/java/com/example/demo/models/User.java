d11a613d264bcdce5ce8f4cb039215bd022a3d5e
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-01-20 13:21:46 +0700

    done user entity

--- src/main/java/com/example/demo/models/User.java
+++ src/main/java/com/example/demo/models/User.java
@@ -10,16 +10,27 @@ import lombok.*;
 @AllArgsConstructor
 @Getter
 @Setter
-public class User {
+public class User extends BaseEntity{
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+    @Column(name="full_name", nullable=false)
     private String fullName;
+    @Column(name="phone_number",length = 10, nullable=false)
     private String phoneNumber;
+    @Column(name="address",length = 200)
     private String address;
+    @Column(name="password",length = 200)
     private String password;
     private Boolean active;
+    @Column(name="date_of_birth")
     private String dateOfBirth;
+    @Column(name="facebook_account_id")
     private int facebookAccountId;
+    @Column(name="google_account_id")
     private int googleAccountId;
+    @ManyToOne
+    @JoinColumn(name="role_id")
+    private Role role;
+
 }
