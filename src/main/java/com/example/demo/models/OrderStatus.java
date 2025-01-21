c8708a5e97c5eb73d0b656520e6bcd8ed584a8e8
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-01-21 14:04:22 +0700

    add Order status

---
+++ src/main/java/com/example/demo/models/OrderStatus.java
@@ -0,0 +1,10 @@
+package com.example.demo.models;
+
+public class OrderStatus {
+    public static final String PENDING = "Pending";
+    public static final String PROCESSING = "processing";
+    public static final String SHIPPED = "shipped";
+    public static final String DELIVERED = "delivered";
+    public static final String CANCEL = "cancel";
+
+}
