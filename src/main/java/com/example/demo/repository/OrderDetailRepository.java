bd672f4c6d54e590e5223d82478049092ba8d552
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-01-27 20:24:22 +0700

    add order repo

--- src/main/java/com/example/demo/repository/OrderDetailRepository.java
+++ src/main/java/com/example/demo/repository/OrderDetailRepository.java
@@ -3,5 +3,8 @@ package com.example.demo.repository;
 import com.example.demo.models.OrderDetail;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface OrderDetailRepository extends JpaRepository<OrderDetail, Long> {
+    List<OrderDetail> findByOrderId(Long orderId);
 }
