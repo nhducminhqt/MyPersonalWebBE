289c4888258a39e36f18eddffbf5cd76c64a8e93
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-01-28 02:09:45 +0700

    done di ngu

---
+++ src/main/java/com/example/demo/services/CategoryService.java
@@ -0,0 +1,37 @@
+package com.example.demo.services;
+
+import com.example.demo.models.Category;
+import com.example.demo.repositories.CategoryRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+@Service
+@RequiredArgsConstructor
+public class CategoryService implements ICategoryService {
+    private final CategoryRepository categoryRepository;
+    @Override
+    public Category createCategory(Category category) {
+        return categoryRepository.save(category);
+    }
+
+    @Override
+    public Category updateCategory(long categoryId, Category category) {
+        return null;
+    }
+
+    @Override
+    public Category deleteCategory(long id) {
+        return null;
+    }
+
+    @Override
+    public List<Category> getAllCategories() {
+        return List.of();
+    }
+
+    @Override
+    public Category getCategoryById(long id) {
+        return null;
+    }
+}
--- src/main/java/com/example/demo/services/ICategoryService.java
+++ src/main/java/com/example/demo/services/ICategoryService.java
@@ -6,7 +6,7 @@ import java.util.List;
 
 public interface ICategoryService {
     Category createCategory(Category category);
-    Category updateCategory(long id,Category category);
+    Category updateCategory(long categoryId,Category category);
     Category deleteCategory(long id);
     List<Category> getAllCategories();
     Category getCategoryById(long id);
