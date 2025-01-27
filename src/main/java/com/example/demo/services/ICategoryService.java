8f43ab862ca8faee1f5cc89360b6b6b4a1605f41
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-01-27 21:31:37 +0700

    add ICategoryService

---
+++ src/main/java/com/example/demo/services/ICategoryService.java
@@ -0,0 +1,13 @@
+package com.example.demo.services;
+
+import com.example.demo.models.Category;
+
+import java.util.List;
+
+public interface ICategoryService {
+    Category createCategory(Category category);
+    Category updateCategory(long id,Category category);
+    Category deleteCategory(long id);
+    List<Category> getAllCategories();
+    Category getCategoryById(long id);
+}
