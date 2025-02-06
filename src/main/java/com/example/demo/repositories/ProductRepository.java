56939e8a5d182f3bd20a81be5e54aae607f0126b
Nguyễn Hoàng Đức Minh <nhducminhqt@gmail.com>
2025-02-06 10:30:53 +0700

    done create

--- src/main/java/com/example/demo/repositories/ProductRepository.java
+++ src/main/java/com/example/demo/repositories/ProductRepository.java
@@ -1,10 +1,9 @@
 package com.example.demo.repositories;
 
 import com.example.demo.models.Product;
-import com.google.api.gax.paging.Page;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.awt.print.Pageable;
 
 public interface ProductRepository extends JpaRepository<Product, Long> {
  boolean existsByName(String name);
